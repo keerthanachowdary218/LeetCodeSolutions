56267a8677fbb754844c1f65acc3414249063316
Keerthana Golla <60168668+keerthanachowdary218@users.noreply.github.com>
2023-12-15 15:03:57 -0600

    Time: 41 ms (34.39%), Space: 16.5 MB (18.47%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -9,20 +9,23 @@ class MyStack:
 		# push new element into queue's tail
         self.queue.append(x)
         # make new element on the head position by rotation
+        ''' 
         for _ in range(len(self.queue)-1):
             self.queue.append( self.queue.popleft() )
+        '''
     def pop(self) -> int:
         """
         Removes the element on top of the stack and returns that element.
         """
 		# pop head element of queue
-        return self.queue.popleft()
+        return self.queue.pop()
     def top(self) -> int:
         """
         Get the top element.
         """
 		# return head element of queue
-        return self.queue[0]
+        n=len(self.queue)-1
+        return self.queue[n]
     def empty(self) -> bool:
         """
         Returns whether the stack is empty.
