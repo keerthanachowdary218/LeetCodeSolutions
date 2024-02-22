8dfe470c4d43ea7738344f3f856d1a9143a8237a
Keerthana Golla <60168668+keerthanachowdary218@users.noreply.github.com>
2024-02-22 10:51:42 -0600

    Time: 42 ms (26.81%), Space: 16.6 MB (53.41%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -11,6 +11,7 @@ class Solution:
             x=x[::-1]
             x=int(x)
             print(x)
+        #they asked to return 0 if after conversion the x is not in range of [-231, 231 - 1]
         if -2**31 <= x and x <= (2**31 - 1):
             return x
         else:
