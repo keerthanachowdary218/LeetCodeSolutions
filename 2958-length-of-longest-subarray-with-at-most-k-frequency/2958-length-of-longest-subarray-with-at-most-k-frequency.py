22309ab7bd3f998c64b6a520204edc15b5d4f73a
Keerthana Golla <60168668+keerthanachowdary218@users.noreply.github.com>
2024-03-28 11:00:52 -0500

    Time: 1253 ms (19.11%), Space: 31.1 MB (37.58%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.py
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.py
@@ -17,22 +17,4 @@ class Solution:
             maxi=max(maxi,right-left+1)
             right+=1
         return maxi
-            
-        '''
-        left, right = 0, 0
-        maxi = 0
-        Hmap = {}
-        while right < len(nums):
-            if nums[right] in Hmap:
-                Hmap[nums[right]] += 1
-            else:
-                Hmap[nums[right]] = 1
-            while Hmap[nums[right]] > k:
-                Hmap[nums[left]] -= 1
-                if Hmap[nums[left]] == 0:
-                    del Hmap[nums[left]]
-                left += 1
-            maxi = max(maxi, right - left + 1)
-            right += 1  
-        return maxi
-'''
+    
