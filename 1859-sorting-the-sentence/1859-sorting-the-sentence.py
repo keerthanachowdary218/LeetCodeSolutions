007b6c67514689479976c33292bc7ac2d25c9f01
Keerthana Golla <60168668+keerthanachowdary218@users.noreply.github.com>
2024-05-17 12:07:21 -0500

    Time: 34 ms (59.68%), Space: 16.6 MB (25.14%) - LeetHub

---
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.py
@@ -0,0 +1,17 @@
+class Solution:
+    def sortSentence(self, s: str) -> str:
+        splits=s.split()
+        mapp={}
+        for i in splits:
+            mapp[i[-1]]=i[:-1]
+        mapp = dict(sorted(mapp.items()))
+        print(mapp)
+        s=''
+        count=0
+        for i in mapp:
+            if count==0:
+                s+=mapp[i]
+            else:
+                 s+=' '+mapp[i]
+            count+=1
+        return s
