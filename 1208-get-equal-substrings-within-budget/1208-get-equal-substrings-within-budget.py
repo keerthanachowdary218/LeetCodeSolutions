d7044c2496e8f529703f2f2a666600e95203687f
Keerthana Golla <60168668+keerthanachowdary218@users.noreply.github.com>
2024-05-28 10:40:30 -0500

    Time: 64 ms (60.58%), Space: 17 MB (100.00%) - LeetHub

--- 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.py
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.py
@@ -16,15 +16,12 @@ class Solution:
         start = 0
         current_cost = 0
         max_length = 0
-
         for end in range(n):
             current_cost += abs(ord(s[end]) - ord(t[end]))
-
             while current_cost > maxCost:
                 current_cost -= abs(ord(s[start]) - ord(t[start]))
                 start += 1
             max_length = max(max_length, end - start + 1)
-        
         return max_length
 
     
