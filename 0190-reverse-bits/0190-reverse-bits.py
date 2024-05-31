28f546eb410c60c421c5fd43876cddc2d22270cc
Keerthana Golla <60168668+keerthanachowdary218@users.noreply.github.com>
2024-05-30 20:29:31 -0500

    Time: 31 ms (86.92%), Space: 16.4 MB (94.51%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,6 +1,6 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        n=bin(n)[2:].zfill(32) #padding with zeroes.
+        n=bin(n)[2:].zfill(32) #converting to binary and padding with zeroes.
         return int(n[::-1],2)
         
         
