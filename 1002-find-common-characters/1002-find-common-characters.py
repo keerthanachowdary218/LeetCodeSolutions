0e61af71c231a2c9a2243825b4c2e222c515e3cc
Keerthana Golla <60168668+keerthanachowdary218@users.noreply.github.com>
2024-06-05 12:56:04 -0500

    Time: 61 ms (10.09%), Space: 16.7 MB (29.50%) - LeetHub

---
+++ 1002-find-common-characters/1002-find-common-characters.py
@@ -0,0 +1,9 @@
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        min_freq = Counter(words[0])
+        
+        #print(min_freq)
+        for word in words:
+            min_freq &= Counter(word)
+            #print(min_freq,word)
+        return list(min_freq.elements())
