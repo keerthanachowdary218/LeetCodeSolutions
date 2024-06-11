9729867bd9ef12631477fe92c01b0ac17f91fd20
Keerthana Golla <60168668+keerthanachowdary218@users.noreply.github.com>
2024-06-11 10:50:23 -0500

    Time: 32 ms (93.75%), Space: 16.5 MB (93.48%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.py
+++ 1122-relative-sort-array/1122-relative-sort-array.py
@@ -1,15 +1,18 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
         mapp={}
+        #creating a mapp with elemnts in order of arr2
         for i in arr2:
             mapp[i]=0
         rest=[]
+        #counting elements in arr2 in arr1
         for i in arr1:
             if i in mapp:
                 mapp[i]+=1
             else:
                 rest.append(i)
         res=[]
+        #appending in result array and also sorting the rest of the elements in array
         for i in mapp:
             for j in range(0,mapp[i]):
                 res.append(i)
