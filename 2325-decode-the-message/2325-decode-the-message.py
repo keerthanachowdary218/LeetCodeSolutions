edcdf2252558a67bb81233335553fe4d3efb9c65
Keerthana Golla <60168668+keerthanachowdary218@users.noreply.github.com>
2024-06-25 11:17:05 -0500

    Time: 46 ms (11.04%), Space: 16.5 MB (65.18%) - LeetHub

---
+++ 2325-decode-the-message/2325-decode-the-message.py
@@ -0,0 +1,19 @@
+class Solution:
+    def decodeMessage(self, key: str, message: str) -> str:
+        hashh={}
+        alpha=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+        currnt=0
+        for i in range(0,len(key)):
+            if key[i] not in hashh and key[i]!=' ':
+                #print(key[i])
+                hashh[key[i]]=alpha[currnt]
+                currnt+=1
+        
+        print(hashh)
+        res=''
+        for i in message:
+            if i==' ':
+                res+=i
+            else:
+                res+=hashh[i]
+        return res
