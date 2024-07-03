429c5858c6fa176d820f7b6819adc4ef4878f8df
Keerthana Golla <60168668+keerthanachowdary218@users.noreply.github.com>
2024-07-03 13:20:02 -0500

    Time: 297 ms (11.32%), Space: 28 MB (12.26%) - LeetHub

--- 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.py
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.py
@@ -5,6 +5,7 @@ class Solution:
         nums.sort()
         print(nums)
         ans = nums[-1] - nums[0]
+        #greedy approach
         #you can change either 3 maximums, or 2 maximums and one minimum and so on.
         for i in range(4):
             print(nums[-(4 - i)] - nums[i])
