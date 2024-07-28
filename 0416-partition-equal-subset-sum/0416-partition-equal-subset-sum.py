2f8c753346a578609aa9db274d9192f9bd7459a9
Keerthana Golla <60168668+keerthanachowdary218@users.noreply.github.com>
2024-07-28 15:16:59 -0500

    Time: 521 ms (66.86%), Space: 16.7 MB (82.81%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -4,9 +4,9 @@ class Solution:
         if total_sum % 2 != 0:
             return False
         target = total_sum // 2
-        dp = [False] * (target + 1)
+        dp = [False] * (target + 1)         
         dp[0] = True
         for num in nums:
-            for j in range(target, num-1, -1):
+            for j in range(target,num-1,-1):
                 dp[j] = dp[j] or dp[j - num]
         return dp[target]
