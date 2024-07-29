8b68fffe11361423e18c3efe1db934984b0a7563
Keerthana Golla <60168668+keerthanachowdary218@users.noreply.github.com>
2024-07-29 13:49:53 -0500

    Time: 39 ms (58.12%), Space: 16.6 MB (50.56%) - LeetHub

---
+++ 0819-most-common-word/0819-most-common-word.py
@@ -0,0 +1,15 @@
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        k = "!?',;."
+        s = paragraph.lower()
+        for char in k:
+            s = s.replace(char, ' ')
+        co = dict(Counter(s.split()))
+        sortedco=sorted(co.items(), key=lambda item: item[1],reverse=True)
+        print(sortedco)
+        for i in sortedco:
+            if i[0] not in banned:
+                return i[0]
+        
+        
+        
