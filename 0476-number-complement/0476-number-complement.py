c87cead5bfc0da73f2e5cb35606a01e762df1fb3
Keerthana Golla <60168668+keerthanachowdary218@users.noreply.github.com>
2024-08-22 14:02:40 -0500

    Time: 36 ms (45.66%), Space: 16.5 MB (58.08%) - LeetHub

--- 0476-number-complement/0476-number-complement.py
+++ 0476-number-complement/0476-number-complement.py
@@ -1,7 +1,11 @@
 class Solution:
     def findComplement(self, num: int) -> int:
         binary = bin(num)[2:]
-        flipped_binary = ''.join(['0' if bit == '1' else '1' for bit in binary])
-        return int(flipped_binary, 2)
-        
+        res=''
+        for i in binary:
+            if i=='0':
+                res+='1'
+            else:
+                res+='0'
+        return int(res,2)
         
