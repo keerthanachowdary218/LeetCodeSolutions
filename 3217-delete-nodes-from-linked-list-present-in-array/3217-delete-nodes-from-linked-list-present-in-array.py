adbcf8fdcb1edb7a890dc5874eaad4cc3e569df2
Keerthana Golla <60168668+keerthanachowdary218@users.noreply.github.com>
2024-09-06 12:02:45 -0500

    Time: 724 ms (51.82%), Space: 54.6 MB (81.13%) - LeetHub

--- 3217-delete-nodes-from-linked-list-present-in-array/3217-delete-nodes-from-linked-list-present-in-array.py
+++ 3217-delete-nodes-from-linked-list-present-in-array/3217-delete-nodes-from-linked-list-present-in-array.py
@@ -5,41 +5,34 @@
 #         self.next = next
 class Solution:
     def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
+        '''
         if not head:
             return None
-
-        # Create a hash set from nums for O(1) lookups
         num_hash = set(nums)
-
-        # Use a dummy node to simplify edge cases
         dummy = ListNode(0)
         dummy.next = head
         prev, node = dummy, head
-
         while node:
             if node.val in num_hash:
-                # Remove the node if its value is in num_hash
                 prev.next = node.next
             else:
-                # Move the prev pointer only when the node is not removed
                 prev = node
             node = node.next
-
-        # Return the updated list starting from dummy.next
         return dummy.next
         '''
         if not head:
             return None
+        num_hash = set(nums)
         node=head.next
         prev=head
         while node:
-            if node.val in set(nums):
+            if node.val in num_hash:
                 prev.next=node.next
             else:
                 prev=node
             node=node.next
-        if head.val in set(nums):
+        if head.val in num_hash:
             head=head.next
         return head
-        '''
+        
         
