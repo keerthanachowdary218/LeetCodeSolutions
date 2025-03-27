01ed41b4cf92ee3471e83b477ba2ceacac141584
Keerthana Golla <60168668+keerthanachowdary218@users.noreply.github.com>
2025-03-27 02:11:34 -0400

    Time: 142 ms (47.62%), Space: 27.8 MB (28.57%) - LeetHub

---
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def minimumIndex(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        '''
+        mapp=defaultdict(int)
+        for i in nums:
+            mapp[i] += 1
+        print(mapp)
+        '''
+        mpp, left = {}, {}
+        for i in nums:
+            mpp[i] = mpp.get(i, 0) + 1
+        
+        for i in range(len(nums) - 1):
+            left[nums[i]] = left.get(nums[i], 0) + 1
+            mpp[nums[i]] -= 1
+
+            if left[nums[i]] * 2 > (i + 1) and mpp[nums[i]] * 2 > (len(nums) - i - 1):
+                return i
+        return -1
+        
