29976ca32141a5db11f9edbde3f5b87c381055fe
Keerthana Golla <60168668+keerthanachowdary218@users.noreply.github.com>
2025-04-05 01:42:00 -0400

    Time: 145 ms (5.71%), Space: 17 MB (28.24%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,19 +1,21 @@
-class Solution:
-    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        # sort the intervals
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        res=[]
         intervals.sort()
-        # initialize a list to return the merged intervals
-        res=[intervals[0]]
-        # from second element to the end of intervals
-        for i in range(1,len(intervals)):
-            # start,end = start and end of current interval
-            start=intervals[i][0]
-            end=intervals[i][1]
-            # if end of current interval overlaps with the end of the last interval in the merged list
-            if start<=res[-1][1]:
-                # update the end of last interval in the merged list
-                res[-1][1]=max(end,res[-1][1])
+        print(intervals)
+        prevstart,prevend=intervals[0]
+        for start, end in intervals[1:]:
+            if prevend >= start:
+                prevend = max(prevend, end)
             else:
-                # else add the current interval in the merged list
-                res.append([start,end])
-        return res      
+                res.append([prevstart, prevend])
+                prevstart, prevend = start, end
+        res.append([prevstart, prevend])
+        return res
+
+
+            
