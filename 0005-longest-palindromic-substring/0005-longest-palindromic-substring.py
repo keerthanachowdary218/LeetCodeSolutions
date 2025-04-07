41b5d77edc9f6ce2335d7292bc34fda3a2bb2159
Keerthana Golla <60168668+keerthanachowdary218@users.noreply.github.com>
2025-04-07 10:28:44 -0400

    Time: 360 ms (59.72%), Space: 12.4 MB (97.44%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,12 +1,15 @@
-class Solution:
-    def longestPalindrome(self, s: str) -> str:
-        result = ''
-        max_length = 0
-        for index in range(len(s)):
-            last_index = len(s) - 1
-            while s[index : last_index + 1] != s[index : last_index + 1][::-1]:
-                last_index = last_index - 1
-            if (last_index - index + 1) > max_length:
-                max_length = (last_index - index + 1)
-                result = s[index : last_index + 1]
-        return result
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        def expand(s,i,j):
+            while i>=0 and j<len(s) and s[i]==s[j]:
+                i-=1
+                j+=1
+            return s[i+1:j]
+        ans=''
+        for i in range(len(s)):
+            ans=max(ans,expand(s,i,i), expand(s,i,i+1), key=len)
+        return ans
