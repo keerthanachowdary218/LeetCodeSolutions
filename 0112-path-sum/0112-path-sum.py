a1da87f669fb4aed246954e53e890bfb2d8fb202
Keerthana Golla <60168668+keerthanachowdary218@users.noreply.github.com>
2025-04-21 11:53:09 -0400

    Time: 4 ms (34.04%), Space: 14.5 MB (12.15%) - LeetHub

--- 0112-path-sum/0112-path-sum.py
+++ 0112-path-sum/0112-path-sum.py
@@ -1,16 +1,42 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        if not root:
-            return False
-        #checking if the root (currnode) is leaf and we reached the targeted sum
-        if not root.left and not root.right and root.val==targetSum:
+class Solution(object):
+    def hasPathSum(self, root, targetSum):
+        """
+        :type root: Optional[TreeNode]
+        :type targetSum: int
+        :rtype: bool
+        """
+        """
+        res=[]
+        def dfs(node,currsum):
+            if not node:
+                return False
+            currsum+=node.val
+            if not node.left and not node.right: 
+                if targetSum==currsum:
+                    return True
+            return dfs(node.left,currsum) or dfs(node.right,currsum)
+        return dfs(root,0)
+        """
+        res=[]
+        def dfs(node,currsum):
+            if not node:
+                return
+            currsum+=node.val
+            if not node.left and not node.right:
+                res.append(currsum)
+            dfs(node.left,currsum)
+            dfs(node.right,currsum)
+        
+        dfs(root,0)
+        if targetSum in res:
             return True
-        leftsum=self.hasPathSum(root.left,targetSum-root.val)
-        rightsum=self.hasPathSum(root.right,targetSum-root.val)
-        return leftsum or rightsum
+        else:
+            return False
+        
+        
