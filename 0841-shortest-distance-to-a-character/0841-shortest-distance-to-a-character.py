cbf7a6d9ee79207966bc92e4ad68769e05a65453
Keerthana Golla <60168668+keerthanachowdary218@users.noreply.github.com>
2025-04-26 21:01:47 -0400

    Time: 51 ms (8.91%), Space: 12.5 MB (79.21%) - LeetHub

--- 0841-shortest-distance-to-a-character/0841-shortest-distance-to-a-character.py
+++ 0841-shortest-distance-to-a-character/0841-shortest-distance-to-a-character.py
@@ -5,6 +5,21 @@ class Solution(object):
         :type c: str
         :rtype: List[int]
         """
+        idx=len(s)
+        res=[len(s)]*len(s)
+        print(res)
+        for i in range(len(s)):
+            if s[i]==c:
+                res[i]=0
+                idx=0
+            else:
+                idx+=1
+                res[i]=min(res[i],idx)
+        print(res)
+        for i in range(len(s)-2,-1,-1):
+            res[i]=min(res[i+1]+1,res[i])
+        return res
+        '''
         shortest_dist = []
         size = len(s)
         if size == 1:
@@ -20,4 +35,4 @@ class Solution(object):
         for idx in range(2, size+1):
             shortest_dist[-idx] = min(shortest_dist[-idx], shortest_dist[-idx+1]+1 ) 
         return shortest_dist
-        
+        '''
