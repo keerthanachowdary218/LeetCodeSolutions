a7ce15a84440cc1e8caef8fca038c849a7255a33
Keerthana Golla <60168668+keerthanachowdary218@users.noreply.github.com>
2025-05-17 14:33:02 -0400

    Time: 0 ms (100%), Space: 12.5 MB (10.9%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,40 +1,40 @@
-class Solution:
-    def sortColors(self, nums: List[int]) -> None:
+class Solution(object):
+    def sortColors(self, nums):
         """
-        Do not return anything, modify nums in-place instead.
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
         """
         '''
-        #brute force--but wouldnt be inplace algorithm
-        zeroes=[]
-        ones=[]
-        twos=[]
-        for i in nums:
-            if i==0:
-                zeroes.append(i)
-            if i==1:
-                ones.append(i)
-            if i==2:
-                twos.append(i)
-        ans=[]
-        ans=zeroes+ones+twos
-        
-        #print(ans)
-        '''
-        # optimal ->in place algorithm
-        z=0
-        h=len(nums)-1
-        i=0
-        while(i<=h):
+        k=0
+        for num in nums:
+            if num==0:
+                k+=1
+        i,j=0,len(nums)-1
+        while i<j:
             if nums[i]==0:
-                nums[z],nums[i]=nums[i],nums[z]
-                z=z+1
-                i=i+1
-            elif nums[i]==1:
-                i=i+1
-            elif nums[i]==2:  
-                nums[i],nums[h]=nums[h],nums[i]
-                h=h-1
-        #return ans
+                i+=1
+            elif nums[i]==2:
+                nums[i],nums[j]=nums[j],nums[i]
+                j-=1
+            else:
+                nums[i],nums[k]=nums[k],nums[i]
+                k+=1
+                i+=1
+            print(nums)
+        return nums
+        '''
+        red, white, blue = 0, 0, len(nums)-1
     
-    
-    
+        while white <= blue:
+            if nums[white] == 0:
+                nums[red], nums[white] = nums[white], nums[red]
+                white += 1
+                red += 1
+            elif nums[white] == 1:
+                white += 1
+            else:
+                nums[white], nums[blue] = nums[blue], nums[white]
+                blue -= 1
+        return nums
+
+            
