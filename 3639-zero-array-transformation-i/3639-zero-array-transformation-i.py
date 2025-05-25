1fde7b05eaa0b915cf377b8d9a0a92d894ce5010
Keerthana Golla <60168668+keerthanachowdary218@users.noreply.github.com>
2025-05-24 20:35:56 -0400

    Time: 71 ms (77.76%), Space: 55.1 MB (56.1%) - LeetHub

--- 3639-zero-array-transformation-i/3639-zero-array-transformation-i.py
+++ 3639-zero-array-transformation-i/3639-zero-array-transformation-i.py
@@ -1,10 +1,5 @@
-class Solution(object):
-    def isZeroArray(self, nums, queries):
-        """
-        :type nums: List[int]
-        :type queries: List[List[int]]
-        :rtype: bool
-        """
+class Solution:
+    def isZeroArray(self, nums: List[int], queries: List[List[int]]) -> bool:
         n = len(nums)
         prefix = [0] * (n + 1)
         for l, r in queries:
@@ -16,15 +11,4 @@ class Solution(object):
             if nums[i] + d > 0:
                 return False
         return True
-        '''
-        for query in queries:
-            for i in range(query[0],query[1]+1):
-                if nums[i]==0:
-                    continue
-                else:
-                    nums[i]-=1
-        for i in nums:
-            if i!=0:
-                return False
-        return True
-        '''
+        
