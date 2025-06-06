52c9ded286b8106c515263ba22d207b981c6a137
Keerthana Golla <60168668+keerthanachowdary218@users.noreply.github.com>
2025-06-06 10:59:04 -0400

    Time: 0 ms (100%), Space: 18.1 MB (21.26%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,9 +1,9 @@
-import itertools
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        k=[[]]
-        for i in range(1,len(nums)+1):
-            #print((itertools.combinations(nums,i)))
-            #there is a difference between append and extend , here we need to use extend insteas of append - check different between append and extend- append(single element) where as extend(iterable)
-            k.extend(list(j) for j in itertools.combinations(nums,i))
-        return (k)
+        subset = [[]]
+        for i in nums:
+            new_subsets = []
+            for s in subset:
+                new_subsets.append(s + [i])
+            subset.extend(new_subsets)
+        return subset
