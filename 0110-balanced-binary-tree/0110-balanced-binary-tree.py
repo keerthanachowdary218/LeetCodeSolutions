e3eae6ae88bf5bd6070ea21aea8a8ff82ab8e15c
Keerthana Golla <60168668+keerthanachowdary218@users.noreply.github.com>
2025-06-07 15:33:39 -0400

    Time: 7 ms (31.3%), Space: 18.9 MB (62.81%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -6,34 +6,15 @@
 #         self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        res = [True]
+        def isH(node):
+            if not node:
+                return 0
+            left = isH(node.left)
+            right = isH(node.right)
+            if abs(left - right) > 1:
+                res[0] = False
+            return max(left, right) + 1
+        isH(root)
+        return res[0]
         
-        #right tree balaced, left tree balanced and height diff is 1
-        '''
-        if not root:
-            return True
-        rightheight=leftheight=0
-        if root.right:
-            rightheight = self.isHeight(root.right)
-        if root.left: 
-            leftheight = self.isHeight(root.left)
-        if (rightheight-leftheight)==0 or (rightheight-leftheight)==1 or (rightheight-leftheight)==-1:
-            return True
-        else:
-            return False
-    def isHeight(self,TreeNode):
-        if not TreeNode:
-            return 0
-        else:
-            return 1+max(self.isHeight(TreeNode.right),self.isHeight(TreeNode.left))
-        '''
-        #lets try optimising 
-        return self.isHeight(root) != -1
-    def isHeight(self, TreeNode):
-        if not TreeNode:
-            return 0
-        left_height = self.isHeight(TreeNode.left)
-        right_height = self.isHeight(TreeNode.right)
-        if left_height == -1 or right_height == -1 or abs(left_height - right_height) > 1:
-            return -1
-        else:
-            return 1 + max(left_height, right_height)
